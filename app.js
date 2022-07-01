da90d20539c5377e9504bea3500ae4db7f517ea4
maniplek <maniplek81@gmail.com>
2022-07-01 18:52:28 +0200

    added google authantication and creating stories

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const path = require('path');
 const express =  require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const passport =require('passport');
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db');
+
 
 //Load config
 dotenv.config({ path: './config/config.env'})
 
+// Passport config
+require('./config/passport')(passport)
+
 connectDB()
 
 const app = express()
+
+//Body parser
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 
 //logging
 if (process.env.NODE_ENV === 'development'){
@@ -21,11 +33,29 @@ if (process.env.NODE_ENV === 'development'){
 app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
+// Sessions
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false,
+        store:  MongoStore.create({
+            mongoUrl: process.env.MONGO_URI,
+        })
+    })
+)
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 //staric folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 //ROUTES
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 3000
 
---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose =  require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    googleId:{
+        type: String,
+        requied: true
+    },
+    displayName:{
+        type: String,
+        requied: true
+    },
+    firstName:{
+        type: String,
+        requied: true
+    },
+    lastName:{
+        type: String,
+        requied: true
+    },
+    image:{
+        type: String      
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    },
+})
+
+module.exports = mongoose.model('User', UserSchema)
